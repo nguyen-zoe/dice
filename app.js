65de15a3af7a6db80ff7f4516589e8049e5d89ca
Zoe Nguyen <93409656+nguyen-zoe@users.noreply.github.com>
2023-12-01 15:06:43 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let ranNum1 = Math.floor(Math.random()* 6 +1);
+let ranImg1= document.querySelector('.img1').setAttribute('src', `./images/dice${ranNum1}.png`);
+let ranNum2 = Math.floor(Math.random()* 6 +1);
+let ranImg2= document.querySelector('.img2').setAttribute('src', `./images/dice${ranNum2}.png`);
+
+
+//declare the winner
+
+document.querySelector('h1').innerHTML= ranNum1 >ranNum2 ? "Player 1 wins" : "Player 2 wins"; 
+
